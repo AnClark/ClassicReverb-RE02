162b1a2105448eb8d47df32868e8036dd5da53ac
AnClark <clarklaw4701@qq.com>
2026-04-02 19:47:10 +0800

    plugin: Update default param value

--- src/Definitions.h
+++ src/Definitions.h
@@ -22,8 +22,8 @@ constexpr DISTRHO::ParameterRanges kParamRanges[kNumParams] = {
     { 150.0f,  0.625f, 640.0f   },   // Room Size, m²
     {  30.0f,  0.0f,   100.0f   },   // Damping, %
     {   0.0f, -150.0f, 150.0f   },   // Pre-delay, ms
-    {   0.0f,  0.0f,   100.0f   },   // Hi Damping, %
-    {  20.0f,  20.0f, 1000.0f   },   // Lo-Cut, Hz
+    {  40.0f,  0.0f,   100.0f   },   // Hi Damping, %
+    {  65.0f,  20.0f, 1000.0f   },   // Lo-Cut, Hz
     {   1.6f, -40.0f,   6.0f    },   // Early Reflections, dB
     {  50.0f,  0.0f,   100.0f   },   // Mix, %
     {   0.0f, -10.0f,  10.0f    },   // Level, dB
